47b5307120b6985211a98ca211531b2aaff66306
joshpress <joshbunny2004@yahoo.com>
2026-05-09 10:49:35 -0700

    add length to dijkstra, add figsize

--- shortest_path.py
+++ shortest_path.py
@@ -8,10 +8,13 @@ class ShortestPath:
 
     def __init__(self, start_coords, end_coords, bounding_buffer=0.05):
 
-        self.G = self._createGraph(start_coords, end_coords, bounding_buffer)
         self.start_coords = start_coords
         self.end_coords = end_coords
 
+        # create graph helper function
+        self.G = self._createGraph(start_coords, end_coords, bounding_buffer)
+
+        # coords[1] is longitude (x), coords[0] is latitude (y)
         self.start = ox.distance.nearest_nodes(
             self.G, X=start_coords[1], Y=start_coords[0])
         self.end = ox.distance.nearest_nodes(
@@ -23,6 +26,7 @@ class ShortestPath:
         east = max(start[1], end[1]) + bounding_buffer
         west = min(start[1], end[1])-bounding_buffer
         print("loading graph")
+        #creating graph from box (left, bottom, right, top)
         G = ox.graph_from_bbox(
             bbox=(west, south, east, north), network_type="drive")
         G = ox.add_edge_speeds(G)
@@ -37,7 +41,7 @@ class ShortestPath:
 
     def a_star(self, traffic_weight):
 
-        # traffic travel time
+        # traffic travel time, edge weight is multiplied
         for u, v, k, data in self.G.edges(data=True, keys=True):
             data['weighted_time'] = data['travel_time'] * traffic_weight
         priority_queue = [(0, self.start)]
@@ -46,19 +50,20 @@ class ShortestPath:
         explored = set()
 
         while priority_queue:
+            #pop node with cheapest code
             _, current = heapq.heappop(priority_queue)
             explored.add(current)
             # if we reached the goal, add the path
             if current == self.end:
                 reconstructed_path = []
                 total_time = cost[self.end]
+                #trace the path back
                 while current in path:
                     reconstructed_path.append(current)
                     current = path[current]
-                reconstructed_path.append(self.start)
                 # total time in seconds
                 total_time = cost[self.end]
-                # return the path, time, and explored
+                # return the path from start to end, time, and explored
                 return reconstructed_path[::-1], total_time, explored
 
             # look at road neighbors
@@ -74,6 +79,7 @@ class ShortestPath:
                     priority = new_cost + self.heuristic(neighbor, self.end)
                     heapq.heappush(priority_queue, (priority, neighbor))
                     path[neighbor] = current
+        print("no path found")
         return None, None, explored
 
     def dijkstra(self, traffic_weight):
@@ -84,11 +90,15 @@ class ShortestPath:
         priority_queue = [(0, self.start)]
         path = {}
         cost = {self.start: 0}
+        explored=set()
+
         # visit the next node and update path
         while priority_queue:
             current_cost, current = heapq.heappop(priority_queue)
-            destinations = self.G[current]
-
+            explored.add(current)
+            #ignore if the cost is more expensive
+            if current_cost > cost.get(current, float("inf")):
+                            continue
             if current == self.end:
                 reconstructed_path = []
                 total_time = cost[self.end]
@@ -99,46 +109,46 @@ class ShortestPath:
                     current = path[current]
                 reconstructed_path.append(self.start)
                 # return the path and the time it takes
-                return reconstructed_path[::-1], total_time
-            
+                return reconstructed_path[::-1], total_time,explored
 
-            if current_cost > cost.get(current, float("inf")):
-                continue
+            
             for neighbor in self.G.neighbors(current):
                 edge_data = self.G.get_edge_data(current, neighbor)
                 weight = min(d["weighted_time"] for d in edge_data.values())
-                #get the new cost
+                # get the new cost
                 new_cost = current_cost+weight
 
-                #if we find a cheaper or new cost, update it
-                if neighbor not in cost or new_cost<cost[neighbor]:
-                    cost[neighbor]=new_cost
-                    heapq.heappush(priority_queue,(new_cost,neighbor))
-                    path[neighbor]=current
-        #if we didn't find a path
-        return None,None
+                # if we find a cheaper or new cost, update it
+                if neighbor not in cost or new_cost < cost[neighbor]:
+                    cost[neighbor] = new_cost
+                    heapq.heappush(priority_queue, (new_cost, neighbor))
+                    path[neighbor] = current
+        # if we didn't find a path
+        print("no path found")
+        return None, None
 
-
-    def displayRoute(self, route,travel_time,explored=None):
-
+    def displayRoute(self, route, travel_time, explored=None,figsize=(10,10)):
+        #for cleaner plotting
         G_undirected = ox.convert.to_undirected(self.G)
-        
 
+        #hiding graph until the points and time are plotted
         fig, ax = ox.plot_graph_route(
-                G_undirected,   route,  route_color="r", route_linewidth=4, node_size=0, orig_dest_size=100,
-                show=False,close=False
-            )
-        #for a* explored visualization
+            G_undirected,   route,  route_color="r", route_linewidth=4, node_size=0,
+            show=False, close=False,figsize=figsize
+        )
+        
+        # for a* explored visualization
         if explored:
             explored_x = [self.G.nodes[n]["x"] for n in explored]
             explored_y = [self.G.nodes[n]["y"] for n in explored]
             ax.scatter(explored_x, explored_y, c="orange",
-                    s=1, alpha=0.4, label="explored")
+                       s=1, alpha=0.4, label="explored")
+        #marking start and end points
         ax.scatter(self.start_coords[1], self.start_coords[0], c="blue",
                    s=250, label="start", zorder=5)
         ax.scatter(self.end_coords[1], self.end_coords[0],
                    c="red", s=250, label="goal", zorder=5, marker="*")
-
+        #travel time box
         ax.annotate(f"{travel_time/60:.2f} minutes", (self.end_coords[1], self.end_coords[0]),
                     ha="center", textcoords="offset points", xytext=(0, 30), bbox=dict(facecolor="white"))
         plt.legend()
